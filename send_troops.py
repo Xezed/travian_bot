0725db3875a8f3db36ed694287ee07c5a87e1ce8
Alexandr Zakharov <sasha750i@mail.ru>
2017-10-25 18:58:45 +0300

    Asynchronous troop-orders. Draft.

--- send_troops.py
+++ send_troops.py
@@ -1,10 +1,13 @@
-from random import randint
+import os
+
 from asyncio import sleep
 from datetime import datetime
+from time import time
 
 from bs4 import BeautifulSoup
 
 from authorization import logged_in_session
+from logger import info_logger_for_future_events
 
 ATTACK_TYPE = {'c': 4}
 
@@ -14,24 +17,39 @@ TROOPS = {'t' + str(i): 0 for i in range(1, 12)}
 class TroopsOrder:
     """Creates an order for troops and send them to concrete point with timing."""
 
-    def __init__(self, barrack_url=None, coords=None):
+    def __init__(self, barrack_url=None, coords=None, time_of_next_raid=0):
         self.barrack_url = barrack_url
         self.coords = dict(coords)
         self.session = None
-        self.time_to_come_back = None
+        self.time_of_next_raid = float(time_of_next_raid)
         self.troops = None
         self.type = dict(ATTACK_TYPE)
 
-    def __call__(self, *args, **kwargs):
+    async def __call__(self, *args, **kwargs):
         self.session = logged_in_session()
+
+        time_to_next_raid = self.time_of_next_raid - time()
+
+        if time_to_next_raid > 0:
+            info_logger_for_future_events('Raid was recently. Next raid in: ', time_to_next_raid)
+            await sleep(time_to_next_raid)
+            await self.__call__()
+
         self.parse_troops_amount()
 
         # If none of troops are available then wait.
         if not self.what_troops_available():
-            sleep(3600)
-            self.__call__()
+            info_logger_for_future_events('Troops are not available. Waiting till... ', 3600)
+            await sleep(3600)
+            await self.__call__()
 
         self.send_troops()
+        self.save_next_raid_time()
+        time_to_next_raid = self.time_of_next_raid - time()
+
+        info_logger_for_future_events('Raiding... Next raid in: ', time_to_next_raid)
+        await sleep(time_to_next_raid)
+        await self.__call__()
 
     def send_troops(self):
         barrack_page = self.session.get(self.barrack_url).text
@@ -51,7 +69,7 @@ class TroopsOrder:
         confirmation = self.session.post(self.barrack_url, data=post_data).text
 
         confirmation_parser = BeautifulSoup(confirmation, 'html.parser')
-        self.parse_time_to_come_back(confirmation_parser)
+        self.parse_time_of_next_raid(confirmation_parser)
 
         hidden_inputs_tags = confirmation_parser.find_all('input', {'type': 'hidden'})
         post_data = {tag['name']: tag['value'] for tag in hidden_inputs_tags}
@@ -73,26 +91,35 @@ class TroopsOrder:
         troops_amount = {name['alt']: amount.text for name, amount in zip(unit_name_tags, unit_amount_tags)}
         self.troops = troops_amount
 
-    def parse_time_to_come_back(self, confirmation_parser):
-        """Compute time to raid and come back to village. Takes parser of confirmation page"""
+    def parse_time_of_next_raid(self, confirmation_parser):
+        """Compute time to next raid. Takes parser of confirmation page"""
         timer_element = confirmation_parser.find('div', class_='at').contents[1]
 
         arrival_time_in_seconds = int(timer_element['value'])
 
         arrival_time = datetime.fromtimestamp(arrival_time_in_seconds) - datetime.now()
 
-        seconds_to_arrive = arrival_time.total_seconds()
-        time_to_come_back = seconds_to_arrive * 2
+        seconds_to_come_back = arrival_time.total_seconds() * 2
 
-        self.time_to_come_back = time_to_come_back
+        come_back_time = time() + seconds_to_come_back
+
+        self.time_of_next_raid = come_back_time + 20000
+
+    def save_next_raid_time(self):
+        """Save coords and time for next raid in file."""
+        with open("raids.txt", "rt") as file_input:
+            with open("new_raids.txt", "wt") as file_output:
+                for line in file_input:
+                    if str(self.coords) in line:
+                        file_output.write(str(self.coords) + ';' + str(self.time_of_next_raid) + '\n')
+                    else:
+                        file_output.write(line)
+        os.rename('new_raids.txt', "raids.txt")
 
     def what_troops_available(self):
         """Determine what type of troops will be sent based on their availability. Return type and amount"""
-        if int(self.troops['Theutates Thunder']) >= 20:
-            return {'t4': "20"}
+        if int(self.troops['Theutates Thunder']) >= 14:
+            return {'t4': "14"}
 
-        if int(self.troops['Phalanx']) >= 10:
-            return {'t1': "10"}
-
-
-# TroopsOrder(barrack_url='https://ts7.travian.com/build.php?tt=2&id=39', coords={'x': '-24', 'y': '-228'})()
+        if int(self.troops['Phalanx']) >= 50:
+            return {'t1': "50"}
